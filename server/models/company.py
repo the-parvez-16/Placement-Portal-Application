9eaa8c2b1140a29a7060cb473de590ca69b13a18
the-parvez-16 <parvez.k.m.16@gmail.com>
2026-08-09 21:55:50 +0530

    feat(backend): company model updated with fields(industry, location, about) and model schema 100%

--- server/models/company.py
+++ server/models/company.py
@@ -1,6 +1,6 @@
 from server.core.extensions import db
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, ForeignKey, Text
 from typing import Optional
 
 class Company(db.Model):
@@ -10,9 +10,13 @@ class Company(db.Model):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False, index=True, unique=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     
+    location: Mapped[Optional[str]] = mapped_column(String(200))
+    industry: Mapped[Optional[str]] = mapped_column(String(100))
+
+    about: Mapped[Optional[str]] = mapped_column(Text)
+
     hr_contact: Mapped[Optional[str]] = mapped_column(String(200))
     website: Mapped[Optional[str]] = mapped_column(String(200))
 
     placement_drives = relationship("PlacementDrive", backref="company", lazy="select", cascade="all, delete-orphan")
-
 
